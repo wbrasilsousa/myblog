fb08b63e5505a4356d878d54ff96868e3b553bd5
Wildney Brasil <wbrasilsousa@gmail.com>
2024-11-27 12:21:59 -0300

    alter database

--- myblog/settings.py
+++ myblog/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_myblog',
+        'USER': 'django',
+        'PASSWORD': 'django-root',
+        'HOST': 'localhost',   # Or the IP address where MySQL is hosted
+        'PORT': '3306',        # MySQL port (default is 3306)
     }
 }
 
