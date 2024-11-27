7caffddd6bd26efd2bd9834857fcb01d6a9b8300
Wildney Brasil <wbrasilsousa@gmail.com>
2024-11-27 14:05:49 -0300

    remove finance lib

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import blog_views, finance_views
+from . import views
 
 
 urlpatterns = [
-    path("", blog_views.blog_index, name="blog_index"),
-    path("post/<slug:slug>/", blog_views.blog_detail, name="blog_detail"),
-    path("category/<category>/", blog_views.blog_category, name="blog_category"),
-
-    path("finance", finance_views.finance, name="finance"),
+    path("", views.blog_index, name="blog_index"),
+    path("post/<slug:slug>/", views.blog_detail, name="blog_detail"),
+    path("category/<category>/", views.blog_category, name="blog_category"),
 ]
--- myblog/settings.py
+++ myblog/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # django-summernote
